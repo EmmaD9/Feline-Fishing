ca254f7113d2b53907004872e7789612394dc3f3
EmmaD9 <maduprey1@gmail.com>
2026-04-02 15:15:11 -0400

    tried to get a "swinging" effect it didn't work

--- Assets/Scripts/FollowMouse2D.cs
+++ Assets/Scripts/FollowMouse2D.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FollowMouse2D : MonoBehaviour
 {
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 10f; // distance from camera
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        mousePos.y = Mathf.Clamp(mousePos.y, 0, Screen.height);
 
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
         transform.position = worldPos;
     }
 }
