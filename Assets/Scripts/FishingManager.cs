b1b5d18ac8898e9c65b9f7aee9b72d2c6169e4d8
maa1446 <maa1446@rit.edu>
2026-04-09 23:53:08 -0400

    Target Area moves for Fishing Bar
    Target area moves after casting and the fish biting

--- Assets/Scripts/FishingManager.cs
+++ Assets/Scripts/FishingManager.cs
@@ -7,12 +7,20 @@ public class FishingManager : MonoBehaviour
 
     private float timer;
     private bool fishing;
+    private bool hooked;
     public GameObject fishingBar;
+    public GameObject targetArea;
+
+    private GameObject currentFishingBar;
+    private GameObject currentTargetArea;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         fishing = false;
+        hooked = false;
+        targetArea.transform.position = new Vector3(-4.22f, -0.08f, 0);
+        fishingBar.transform.position = new Vector3(-4.224f, -0.29f, 0);
     }
 
     // Update is called once per frame
@@ -22,6 +30,14 @@ public class FishingManager : MonoBehaviour
         {
             StartFishing();
             fishing = true;
+        }
+        if (hooked)
+        {
+            Debug.Log(currentTargetArea.transform.position);
+            //Vector3 currentTarget = targetArea.transform.position;
+            //currentTarget.y += 0.001f;
+            //targetArea.transform.position = currentTarget;
+            currentTargetArea.transform.position += new Vector3(0, 0.001f, 0);
         }
     }
 
@@ -37,5 +53,8 @@ public class FishingManager : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         Debug.Log("Timer Finished");
+        currentFishingBar = Instantiate(fishingBar);
+        currentTargetArea = Instantiate(targetArea);
+        hooked = true;
     }
 }
