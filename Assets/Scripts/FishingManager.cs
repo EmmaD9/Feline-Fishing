0bd219f3574c43834cb7388e65c08948afd6caa8
maa1446 <maa1446@rit.edu>
2026-04-10 00:30:57 -0400

    Player can move their bar
    Player left clicks move the blue player region, allowing them to align it with the green target region

--- Assets/Scripts/FishingManager.cs
+++ Assets/Scripts/FishingManager.cs
@@ -11,11 +11,14 @@ public class FishingManager : MonoBehaviour
     private bool hooked;
     public GameObject fishingBar;
     public GameObject targetArea;
+    public GameObject playerArea;
 
     private GameObject currentFishingBar;
     private GameObject currentTargetArea;
+    private GameObject currentPlayerArea;
 
     private float barMovement;
+    private float playerMovement;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,7 +27,9 @@ public class FishingManager : MonoBehaviour
         hooked = false;
         targetArea.transform.position = new Vector3(-4.22f, -0.08f, 0);
         fishingBar.transform.position = new Vector3(-4.22f, -0.29f, 0);
+        playerArea.transform.position = new Vector3(-4.22f, -0.31f, 0);
         barMovement = 0.005f;
+        playerMovement = 0.01f;
     }
 
     // Update is called once per frame
@@ -42,6 +47,24 @@ public class FishingManager : MonoBehaviour
                 barMovement *= -1;
             }
             currentTargetArea.transform.position += new Vector3(0, barMovement, 0);
+
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                Debug.Log("Clicked");
+                currentPlayerArea.transform.position += new Vector3(0, playerMovement * 100, 0);
+                if(currentPlayerArea.transform.position.y >= 2.9)
+                {
+                    currentPlayerArea.transform.position = new Vector3(currentPlayerArea.transform.position.x, 2.9f, 0);
+                }
+            }
+            else
+            {
+                currentPlayerArea.transform.position -= new Vector3(0, playerMovement, 0);
+                if (currentPlayerArea.transform.position.y <= -3.4)
+                {
+                    currentPlayerArea.transform.position = new Vector3(currentPlayerArea.transform.position.x, -3.4f, 0);
+                }
+            }
         }
     }
 
@@ -59,6 +82,7 @@ public class FishingManager : MonoBehaviour
         Debug.Log("Timer Finished");
         currentFishingBar = Instantiate(fishingBar);
         currentTargetArea = Instantiate(targetArea);
+        currentPlayerArea = Instantiate(playerArea);
         hooked = true;
     }
 }
