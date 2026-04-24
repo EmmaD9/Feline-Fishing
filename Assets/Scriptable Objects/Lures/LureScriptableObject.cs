3676bc30109b6c97f5f04b3d77acd181ca810843
EmmaD9 <maduprey1@gmail.com>
2026-04-24 13:44:44 -0400

    updated the lures!!!

--- Assets/Scriptable Objects/Lures/LureScriptableObject.cs
+++ Assets/Scriptable Objects/Lures/LureScriptableObject.cs
@@ -10,10 +10,10 @@ public class LureScriptableObject : ScriptableObject
     [SerializeField]
     private float cost;
     [SerializeField]
-    private Texture2D texture;
+    private Sprite sprite;
 
     public string LureName { get => lureName; }
     public string FishAttracted { get => fishAttracted; }
     public float Cost { get => cost;}
-    public Texture2D Texture { get => texture;}
+    public Sprite Sprite { get => sprite;}
 }
